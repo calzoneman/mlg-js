3cd90016d267962114d70e579c14867770823f1c
calzoneman <calzoneman@gmail.com>
2013-05-30 16:40:05 -0400

    Revise the way screen handles mods

--- screen.js
+++ screen.js
@@ -4,26 +4,24 @@ var Screen = function(canvas) {
     canvas.moddisabled = {};
     canvas.modticks = {};
 
-    canvas.modOn = function(mod) {
-        this.mods[mod] = true;
-        if(mod in this.modenabled) {
-            this.modenabled[mod].bind(this)();
+    canvas.modOn = function(name) {
+        if(name in this.mods) {
+            this.mods[name].enabled = true;
+            this.mods[name].init(this);
         }
     };
 
-    canvas.modOff = function(mod) {
-        this.mods[mod] = false;
-        if(mod in this.moddisabled) {
-            this.moddisabled[mod].bind(this)();
+    canvas.modOff = function(name) {
+        if(name in this.mods) {
+            this.mods[name].enabled = false;
+            this.mods[name].takedown(this);
         }
     };
 
     canvas.modTick = function() {
         for(var mod in this.mods) {
-            if(this.mods[mod]) {
-                if(mod in this.modticks) {
-                    this.modticks[mod].bind(this)();
-                }
+            if(this.mods[mod].enabled) {
+                this.mods[mod].tick(this);
             }
         }
     };
@@ -35,57 +33,60 @@ var Screen = function(canvas) {
 
 function setupMods(screen) {
     // Rotate & scale
-    screen.modenabled["rotscale"] = function() {
-        this.ang = 0;
-        this.angdelta = 0.06;
-        this.scale = 1;
-        this.scaledelta = 1.02;
-    }
+    screen.mods["rotscale"] = {
+        enabled: false,
+        init: function(screen) {
+            this.ang = 0;
+            this.angdelta = 0.06;
+            this.scale = 1;
+            this.scaledelta = 1.02;
+        },
+        tick: function(screen) {
+            this.ang += this.angdelta;
+            if(Math.abs(this.ang) > Math.PI / 8) {
+                this.angdelta *= -1;
+            }
 
-    screen.modticks["rotscale"] = function() {
-        this.ang += this.angdelta;
-        if(Math.abs(this.ang) > Math.PI / 8) {
-            this.angdelta *= -1;
+            this.scale *= this.scaledelta;
+            if(this.scale > 1.15 || this.scale < 0.85) {
+                this.scaledelta = 1 / this.scaledelta;
+            }
+
+            $(screen).css("transform",
+                "rotate(" + this.ang + "rad) scale(" + this.scale + ")");
+        },
+        takedown: function(screen) {
+            $(screen).css("transform", "");
         }
-
-        this.scale *= this.scaledelta;
-        if(this.scale > 1.15 || this.scale < 0.85) {
-            this.scaledelta = 1 / this.scaledelta;
-        }
-
-        $(this).css("transform", "rotate(" + this.ang + "rad) scale(" + this.scale + ")");
-    }
-
-    screen.moddisabled["rotscale"] = function() {
-        $(this).css("transform", "");
-    }
+    };
 
     // Shake
-    screen.modenabled["shake"] = function() {
-        this.shakex = 11;
-        this.shakey = 0;
-        this.shakedx = 0;
-        this.shakedy = 0;
-        $(this).css("position", "relative");
-    }
-
-    screen.modticks["shake"] = function() {
-        this.shakex += this.shakedx;
-        this.shakey += this.shakedy;
-        var a = Math.atan2(this.shakey, this.shakex);
-        var r = Math.sqrt(this.shakex*this.shakex + this.shakey*this.shakey);
-        if(r > 10) {
-            var b = a + Math.random() * Math.PI/2 + Math.PI/2;
-            this.shakedx = 10 * Math.cos(b);
-            this.shakedy = 10 * Math.sin(b);
+    screen.mods["shake"] = {
+        enabled: false,
+        init: function(screen) {
+            this.shakex = 11;
+            this.shakey = 0;
+            this.shakedx = 0;
+            this.shakedy = 0;
+            $(screen).css("position", "relative");
+        },
+        tick: function(screen) {
+            this.shakex += this.shakedx;
+            this.shakey += this.shakedy;
+            var a = Math.atan2(this.shakey, this.shakex);
+            var r = Math.sqrt(this.shakex*this.shakex + this.shakey*this.shakey);
+            if(r > 10) {
+                var b = a + Math.random() * Math.PI/2 + Math.PI/2;
+                this.shakedx = 10 * Math.cos(b);
+                this.shakedy = 10 * Math.sin(b);
+            }
+            $(screen).css("left", this.shakex + "px");
+            $(screen).css("top", this.shakey + "px");
+        },
+        takedown: function(screen) {
+            $(screen).css("left", "");
+            $(screen).css("top", "");
+            $(screen).css("position", "");
         }
-        $(this).css("left", this.shakex + "px");
-        $(this).css("top", this.shakey + "px");
-    }
-
-    screen.moddisabled["shake"] = function() {
-        $(this).css("left", "");
-        $(this).css("top", "");
-        $(this).css("position", "");
-    }
+    };
 }
